bc9b5596476e34b2f7cc9541cbe3cf11eb75af82
Shubhang-sharma <shubhangsharma123@gmail.com>
2021-08-04 03:04:00 +0530

    Bg Animations and stuff

---
+++ Bg/sketch.js
@@ -0,0 +1,34 @@
+var canvas;
+let nodes = [];
+let DNA;
+let z;
+let j = 0;
+function preload(){
+  DNA = loadImage('Bg/DNA.png');
+  bbg = loadImage('Bg/blurbg.png');
+}
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0,0);
+  tint(200,120);
+  image(bbg,0,0);
+  filter(BLUR,3);
+  canvas.style('z-index','-1');
+  for(j=0;j<50;j++){
+      x = random(0,windowWidth);
+      y = random(0,windowHeight);
+      z = random(10,30);
+      nodes[j] = new Node(x,y,z);
+  }
+}
+function draw(){
+  background(20);
+  for (i = 0; i < nodes.length; i++) {
+    nodes[i].make();
+    nodes[i].random();
+    // nodes[i].dsize();
+  // image(DNA,00,0);
+  }
+
+  // setAlpha(50);
+}
