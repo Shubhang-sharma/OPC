1481a48b76ededfb371258b9fb8f28e97289afa4
Shubhang-sharma <shubhangsharma123@gmail.com>
2021-08-06 15:29:50 +0530

    bootsrap and flex

--- Bg/sketch.js
+++ Bg/sketch.js
@@ -8,18 +8,22 @@ function preload(){
   bbg = loadImage('Bg/blurbg.png');
 }
 function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
-  canvas.position(0,0);
+  canvas = createCanvas(0.8*windowWidth,0.8*windowHeight);
   tint(200,120);
   image(bbg,0,0);
   filter(BLUR,3);
-  canvas.style('z-index','-1');
+  // canvas.style('z-index','-1');
+  // canvas.style('position','absolute');
+  // canvas.style('top','230px')
   for(j=0;j<15;j++){
-      x = random(0,windowWidth);
-      y = random(0,windowHeight);
+      x = random(0,0.8*windowWidth);
+      y = random(0.2*windowHeight,0.8*windowHeight);
       z = random(10,30);
       nodes[j] = new Node(x,y,z);
   }
+}
+function windowResized() {
+  resizeCanvas(0.8*windowWidth,0.8* windowHeight);
 }
 function draw(){
   background(0);
