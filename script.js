f4d10addcf80700f993f490ec1b1a17bd032deff
Shubhang-sharma <shubhangsharma123@gmail.com>
2021-08-08 12:32:21 +0530

    bef react-p5 impl

---
+++ script.js
@@ -0,0 +1,11 @@
+function hide() {
+  document.querySelector('.sidebar').style.width = "0%";
+  document.querySelector('.sidebar').style.overflow = "hidden";
+
+  // document.getElementsByClassName('.body').style.marginRight = "0";
+}
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     document.querySelector(".ans").onclick = hide;
+//
+// });
